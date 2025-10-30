199e90a13ad25620302f0c316cc3ae7d1e6a811d
corgifist <sxuare.science@gmail.com>
2025-10-30 21:47:03 +0300

    feat: integrated vcpkg package manager

--- Examples/MiniBrowser/MIniBrowser.cpp
+++ Examples/MiniBrowser/MIniBrowser.cpp
@@ -3,5 +3,12 @@
 int main(int argc, char **argv) {
 	GestaltC();
 	Gestalt::NamespaceTest();
+#if PLATFORM(WIN)
+	std::cout << "Windows platform" << std::endl;
+#elif PLATFORM(LINUX)
+	std::cout << "Linux platform" << std::endl;
+#elif PLATFORM(DARWIN)
+	std::cout << "Darwin platform" << std::endl;
+#endif
 	return 0;
 }
